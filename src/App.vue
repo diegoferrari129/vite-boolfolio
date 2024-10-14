9886aaa9d73230592448e12942e9daf40d4b3d29
diego ferrari <167826265+diegoferrari129@users.noreply.github.com>
2024-10-15 01:52:25 +0200

    [FEAT] created AppProjects component and added api call for projects. Displayed in console log

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
-<template>
-  <div>
-    
-  </div>
-</template>
 <script>
+import AppProjects from './components/AppProjects.vue';
+
 export default {
-  
+  components:{
+    AppProjects,
+  }
 }
 </script>
-<style lang="">
-  
+
+<template>
+  <div>
+    <AppProjects />
+  </div>
+</template>
+
+<style lang="scss">
+  @import './styles/generals.scss';
 </style>
